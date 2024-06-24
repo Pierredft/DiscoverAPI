dd3fe9ab659a6ef9b5859a858001a1111d1cede8
Pierre DEFAUQUET <pierredefauquet@device-38.home>
2024-06-24 15:27:36 +0200

    fin de la p47

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,27 +3,40 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
+use App\Entity\User;
 use App\Entity\Author;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
     public function load(ObjectManager $manager): void
     {
+        $user = new User();
+        $user->setEmail("user@bookapi.com");
+        $user->setRoles(["ROLE_USER"]);
+        $user->setPassword($this->userPasswordHasher->hashPassword($user,"password"));
+        $manager->persist($user);
         
-        // for ($i =0; $i < 20; $i++) {
-        //     $livre = new Book;
-        //     $livre->setTitle('Livre' . $i);
-        //     $livre->setCoverText('Quatrième de couverture numéro :' . $i);
-        //     $manager->persist($livre);
-        // }
-
+        $userAdmin = new User();
+        $userAdmin->setEmail("admin@bookapi.com");
+        $userAdmin->setRoles(["ROLE_ADMIN"]);
+        $userAdmin->setPassword($this->userPasswordHasher->hashPassword($userAdmin,"password"));
+        $manager->persist($userAdmin);
+        $this->userPasswordHasher->hashPassword($userAdmin,"password");
+        
         $listAuthor = [];
         for ($i = 0; $i < 10; $i++) {
             $author = new Author();
             $author->setFirstName('Prénom' . $i);
-            $author->setFirstname('Nom' . $i);
+            $author->setFirstName('Nom' . $i);
             $manager->persist($author);
             $listAuthor[] = $author;
         }
