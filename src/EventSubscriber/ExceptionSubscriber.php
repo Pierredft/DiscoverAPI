e0e02cebc3426685fb2bb0f703cd796f393de649
Pierre DEFAUQUET <pierredefauquet@device-38.home>
2024-06-24 14:44:30 +0200

    fin de la p37

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $execption = $event->getThrowable();
+        if ($execption instanceof HttpException) {
+            $data =[
+                'status' => $execption->getStatusCode(),
+                'message' => $execption->getMessage()
+            ];
+            $event->setResponse(new JsonResponse($data));
+        }else {
+            $data =[
+                'status ' => 500,
+                'message'=> $execption->getMessage()
+            ];
+            $event->setResponse(new JsonResponse($data));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
